a302fcf73bb41cea93a312a3c9d7f9d8aa3f955d
Wybie <lyrncofyn@gmail.com>
2026-03-31 23:56:56 +0800

    feat: add fullName utility function for personnel name formatting

---
+++ resources/js/utils.ts
@@ -0,0 +1,7 @@
+import { Personnel } from "./types";
+
+export function fullName(personnel: Personnel): string {
+    return [personnel.first_name, personnel.middle_name, personnel.last_name]
+        .filter(Boolean)
+        .join(' ');
+}
