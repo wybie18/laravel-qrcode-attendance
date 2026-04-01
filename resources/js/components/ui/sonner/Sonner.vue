bb42751163e92f49d7204f27f1a81af4962d6915
Wybie <lyrncofyn@gmail.com>
2026-04-01 23:25:41 +0800

    fix: ensure Sonner styles are imported correctly in Sonner.vue

--- resources/js/components/ui/sonner/Sonner.vue
+++ resources/js/components/ui/sonner/Sonner.vue
@@ -2,6 +2,7 @@
 import type { ToasterProps } from "vue-sonner"
 import { CircleCheckIcon, InfoIcon, Loader2Icon, OctagonXIcon, TriangleAlertIcon, XIcon } from "lucide-vue-next"
 import { Toaster as Sonner } from "vue-sonner"
+import "vue-sonner/style.css"
 import { cn } from "@/lib/utils"
 
 const props = defineProps<ToasterProps>()
