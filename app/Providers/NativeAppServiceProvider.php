79ff3b9f87a02c00aeb2c3de9fc933fd485e6b60
Wybie <lyrncofyn@gmail.com>
2026-04-02 22:37:05 +0800

    feat: add NativeAppServiceProvider and nativephp configuration file

---
+++ app/Providers/NativeAppServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+use Native\Desktop\Facades\Window;
+use Native\Desktop\Contracts\ProvidesPhpIni;
+use Native\Desktop\Facades\Menu;
+
+class NativeAppServiceProvider implements ProvidesPhpIni
+{
+    /**
+     * Executed once the native application has been booted.
+     * Use this method to open windows, register global shortcuts, etc.
+     */
+    public function boot(): void
+    {
+        Menu::create(
+            Menu::app(),
+            Menu::file(),
+            Menu::edit(),
+            Menu::view(),
+            Menu::window(),
+            Menu::make(
+                Menu::route('login')->label('Go to Login')
+            )->label('Admin')
+        );
+
+        Window::open();
+    }
+
+    /**
+     * Return an array of php.ini directives to be set.
+     */
+    public function phpIni(): array
+    {
+        return [
+        ];
+    }
+}
