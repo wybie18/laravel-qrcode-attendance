2da91744a3fabe003d0007f8d9b4cc7863a26d00
Wybie <lyrncofyn@gmail.com>
2026-04-03 14:38:13 +0800

    feat: add Email navigation item to settings sidebar

--- resources/js/layouts/settings/Layout.vue
+++ resources/js/layouts/settings/Layout.vue
@@ -6,6 +6,7 @@ import { Separator } from '@/components/ui/separator';
 import { useCurrentUrl } from '@/composables/useCurrentUrl';
 import { toUrl } from '@/lib/utils';
 import { edit as editAppearance } from '@/routes/appearance';
+import { edit as editEmail } from '@/routes/email';
 import { edit as editProfile } from '@/routes/profile';
 import { edit as editSecurity } from '@/routes/security';
 import type { NavItem } from '@/types';
@@ -18,6 +19,10 @@ const sidebarNavItems: NavItem[] = [
     {
         title: 'Security',
         href: editSecurity(),
+    },
+    {
+        title: 'Email',
+        href: editEmail(),
     },
     {
         title: 'Appearance',
